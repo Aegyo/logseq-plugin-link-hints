a5fb82b29ccfee799c6b80ed1c55bb30acc6f6bd
Jessica Chen <jessica.scoltock@protonmail.com>
2022-04-01 01:51:21 +0100

    Make link hints appear on hotkey press

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import '@logseq/libs';
 async function main() {
   logseq.App.showMsg('Link Hints loaded!');
 
-  const visibleLinks: Map<Element, DOMRectReadOnly> = new Map();
+  const visibleLinks = new Set<Element>();
   
   const observeRoot = parent.document.getElementById('app-container');
   if (observeRoot === null) {
@@ -11,11 +11,11 @@ async function main() {
     return;
   }
 
-  observeLinkChanges(observeRoot, ({target, isIntersecting, boundingClientRect, rootBounds}: IntersectionObserverEntry) => {
-    if (isIntersecting) visibleLinks.set(target, boundingClientRect);
-    else visibleLinks.delete(target);
+  const linkContainer = await addLinkContainer();
 
-    console.log(target, isIntersecting, boundingClientRect, rootBounds);
+  observeLinkChanges(observeRoot, ({target, isIntersecting}: IntersectionObserverEntry) => {
+    if (isIntersecting) visibleLinks.add(target,);
+    else visibleLinks.delete(target);
   });
 
   logseq.App.registerCommandPalette(
@@ -27,20 +27,48 @@ async function main() {
         binding: 'f',
       },
     },
-  () => {
+  async () => {
     console.log('pressed!')
     const fragment = document.createDocumentFragment();
     const gen = keyGenerator();
     
-    visibleLinks.forEach((boundingRect, el) => {
+    // previous observations don't seem to give us enough info to know where the links are now
+    // so observe again (instead of getBoundingClientRect directly which has performance issues)
+    const currentPosMap = new Map<Element, DOMRectReadOnly>()
+    const tmpIntersectionObs = new IntersectionObserver(entries => {
+      entries.forEach(({target, isIntersecting, boundingClientRect}) => {
+        if (isIntersecting) currentPosMap.set(target, boundingClientRect);
+      });
+    });
+    visibleLinks.forEach(link => tmpIntersectionObs.observe(link))
+
+
+    await delay(0);
+    console.log('posMap', currentPosMap);
+
+    currentPosMap.forEach((boundingRect, el) => {
       fragment.appendChild(createHint(el, boundingRect, gen.next().value));
     });
 
-    parent.document.documentElement.appendChild(fragment);
+    linkContainer.appendChild(fragment);
     console.log("added hints", fragment);
 
     beginCaptureInput();
   });
+
+  // TODO find proper colors for hints
+  logseq.provideStyle(`
+    #link-hints-container {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    .link-hint {
+      background: white;
+      position: relative;
+    }
+  `);
 }
 
 function observeLinkChanges(
@@ -79,8 +107,16 @@ function findLinks(node: Node) {
   return (node as Element)?.querySelectorAll('a[data-ref]') || [];
 }
 
-function onHotkey(callback) {
-  // TODO implement
+function addLinkContainer() {
+  logseq.provideUI({
+    key: 'link-hints-container',
+    path: 'body',
+    template: '<div id="link-hints-container"></div>'
+  });
+
+  return new Promise<Element>((resolve) => {
+    setTimeout(() => resolve(parent.document.getElementById('link-hints-container') as Element), 0)
+  });
 }
 
 function* keyGenerator(): Generator<string> {
@@ -93,6 +129,7 @@ function createHint(element: Element, boundingRect: DOMRectReadOnly, keys: strin
   const hint = document.createElement('div');
 
   hint.textContent = keys;
+  hint.className = 'link-hint';
 
   hint.style.position = 'relative';
   hint.style.top = `${boundingRect.top}px`;
@@ -103,6 +140,10 @@ function createHint(element: Element, boundingRect: DOMRectReadOnly, keys: strin
 
 function beginCaptureInput() {
   // TODO implement
+}
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 logseq.ready(main).catch(console.error);
